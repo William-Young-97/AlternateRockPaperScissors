f711dc7c03b327ba0df4016c16741f128c472202
William Young <will.young@engineerbetter.com>
2022-09-28 16:58:59 +0100

    Inital commit

---
+++ AlternateRockPaperScissors/AlternateRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlternateRockPaperScissorsApp.swift
+//  AlternateRockPaperScissors
+//
+//  Created by William Young on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlternateRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
