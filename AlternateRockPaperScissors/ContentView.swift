355728f4cb4c0d5a30696db5f29bf2cca2bbaa2c
William Young <wadyoung@outlook.com>
2022-09-29 21:04:05 +0100

    Add game logic

--- AlternateRockPaperScissors/ContentView.swift
+++ AlternateRockPaperScissors/ContentView.swift
@@ -14,14 +14,6 @@ struct ContentView: View {
     @State private var score = 0
     @State private var win = Bool.random()
     @State private var userInput = ""
-
-//    Text("Your opponent has select:")
-//        Button {
-//        } label: {
-//            Image(rockPaperScissors[opponentsSelection])
-//                .resizable()
-//                .frame(width: 50, height: 50, alignment: .center)
-//        }
     
     var body: some View {
         NavigationView {
@@ -31,7 +23,8 @@ struct ContentView: View {
                     HStack{
                         ForEach(0..<3) { number in
                             Button {
-                                self.userInput = winGameLogic(number)
+                                self.userInput = gameLogic(number)
+                                self.opponentsSelection = Int.random(in: 0...2)
                                 win.toggle()
                             } label: {
                                 Image(rockPaperScissors[number])
@@ -40,6 +33,16 @@ struct ContentView: View {
                             }
                         }
                         Text(userInput)
+                    }
+                    VStack {
+                        Text("Your opponent has selected:")
+                            Button {
+                            } label: {
+                                Image(rockPaperScissors[opponentsSelection])
+                                    .resizable()
+                                    .frame(width: 50, height: 50, alignment: .center)
+                            }
+                        Text("Your score is: \(score)/10")
                     }
                         .navigationTitle("Rock, Paper, Scissors")
                 } else {
@@ -47,7 +50,8 @@ struct ContentView: View {
                     HStack{
                         ForEach(0..<3) { number in
                             Button {
-                                // Lose game logic
+                                self.userInput = gameLogic(number)
+                                self.opponentsSelection = Int.random(in: 0...2)
                                 win.toggle()
                             } label: {
                                 Image(rockPaperScissors[number])
@@ -57,26 +61,70 @@ struct ContentView: View {
                         }
                         Text(userInput)
                     }
+                    VStack {
+                        Text("Your opponent has selected:")
+                            Button {
+                            } label: {
+                                Image(rockPaperScissors[opponentsSelection])
+                                    .resizable()
+                                    .frame(width: 50, height: 50, alignment: .center)
+                            }
+                        Text("Your score is: \(score)/10")
+                    }
                         .navigationTitle("Rock, Paper, Scissors")
-                }
+                    }
                 }
             }
         }
     
-    func winGameLogic(_ number: Int) -> String {
-        if number == opponentsSelection {
-            score += 1
-        }
-        if rockPaperScissors[number] == "Paper" {
-            return "Clicked paper"
-        } else if rockPaperScissors[number] == "Scissors" {
-            return "Clicked Scissors"
-        }
-        else {
-            return "Clicked Rock"
+    func gameLogic(_ number: Int) -> String {
+        switch (rockPaperScissors[number], rockPaperScissors[opponentsSelection], win) {
+            // Player picks correctly on win turn
+            case ("Paper", "Rock", true):
+              score += 1
+              return "You scored a point!"
+            case ("Rock", "Scissors", true):
+              score += 1
+              return "You scored a point!"
+            case ("Scissors", "Paper", true):
+              score += 1
+              return "You scored a point!"
+            // Player picks incorrectly on win turn
+            case ("Paper", "Scissors", true):
+              score -= 1
+              return "You lost a point!"
+            case ("Rock", "Paper", true):
+              score -= 1
+              return "You lost a point!"
+            case ("Scissors", "Rock", true):
+              score -= 1
+              return "You lost a point!"
+            // Player picks correctly on lose turn
+            case ("Rock", "Paper", false):
+              score += 1
+              return "You scored a point!"
+            case ("Scissors", "Rock", false):
+              score += 1
+              return "You scored a point!"
+            case ("Paper", "Scissors", false):
+              score += 1
+              return "You scored a point!"
+            // Player picks incorrectly on lose turn
+            case ("Paper", "Rock", false):
+              score -= 1
+              return "You lost a point!"
+            case ("Rock", "Scissors", false):
+              score -= 1
+              return "You lost a point!"
+            case ("Scissors", "Paper", false):
+              score -= 1
+              return "You lost a point!"
+            // Player picks the same choice
+            default:
+              return "It's a draw!"
+            }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
